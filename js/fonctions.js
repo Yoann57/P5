39227eb8122b4153152fdb2c94a32946f4807b16
yoann57 <kalou33@yahoo.fr>
2021-05-10 17:17:16 +0200

    msg panier js

--- js/fonctions.js
+++ js/fonctions.js
@@ -5,6 +5,16 @@
         var count = div.textContent * 1 - 1;
         div.textContent = count;
         if (count <= 0) {
-            window.location.replace("index.html");
+            window.location.replace("../index.html");
         }
     }, 1000);
+
+//Message ajouté au panier
+
+function ajouterpanier() {
+    const msg="PRODUIT(S) AJOUTÉ(S) AU PANIER";
+    console.log(msg)
+    alert(msg);
+}
+
+
